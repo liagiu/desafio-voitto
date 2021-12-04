46f0353e36251ba56981a4dc540e81124d2035c3
Giulia <giu.olivers@gmail.com>
2021-12-04 14:14:38 -0300

    Resolve teste lógico

--- web/src/helpers/logicFunctions/index.ts
+++ web/src/helpers/logicFunctions/index.ts
@@ -1,4 +1,16 @@
-export const getNoFibonacciNumber = (n: number): number => {
-  return 4;
-  //to do
+import { useIsFocusVisible } from '@material-ui/core';
+
+export const getNoFibonacciNumber = (n: number) => {
+  let prev2 = 1,
+    prev1 = 2,
+    fib = 3;
+
+  while (n > 0) {
+    prev2 = prev1;
+    prev1 = fib;
+    fib = prev2 + prev1;
+    n -= prev2 - 1;
+  }
+  n += prev2 - 1;
+  return n + prev1;
 };
